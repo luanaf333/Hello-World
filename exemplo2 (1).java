3430d5bc99b4bc1b059a7ce275175d524b45954c
Luana França <francalua0@gmail.com>
2026-03-03 20:25:14 -0300

    Add files via upload

---
+++ exemplo2 (1).java
@@ -0,0 +1,8 @@
+public class exemplo2 {
+    public static void main (String[] args){
+        double nota = 8.567;
+        System.out.printf("A nota final foi %.2f.%n", nota);
+        
+    }
+    
+}
