9a2ee9386bf8b508ded1701b20cb1f79aa97cfee
Luana França <francalua0@gmail.com>
2026-03-03 20:25:43 -0300

    Add files via upload

---
+++ Exemplo3.java
@@ -0,0 +1,8 @@
+public class exemplo3 {
+    public static void main(String[] args){
+        String nome = "Ana";
+        int idade = 20;
+        System.out.printf("Nome: %-10s idade: %d%n", nome, idade);
+        
+    }
+}
